2d6d7de6b671369fe0a1d14f35bae3748604adaf
vinnosh <34763854+vinnosh@users.noreply.github.com>
2024-10-26 17:52:21 +0800

    Update robot.h

--- robot.h
+++ robot.h
@@ -2,12 +2,7 @@
 Program: robot.cpp / robot.h
 Course: Object Oriented Programming and Data Structures
 Trimester: 2410
-Name: Vinnosh Rau A/L Samudram , Suah Min Yew , Uzair Akhyar
-ID: 1211108264 , 1221303768 , 1211102576
-Lecture Section: TC4L
-Tutorial Section: TT7L
-Email: 1211108264@soffice.mmu.edu.my , 1221303768@soffice.mmu.edu.my , 1211102576@soffice.mmu.edu.my
-Phone: 012-2185040 , 011-23637292 , 010-3644346
+
 ***|***|****/
 #ifndef ROBOT_H
 #define ROBOT_H
